d59b6b7487a2201496711e3143fea338288e2fa6
otto-menza <otoniel.jump@gmail.com>
2023-11-20 21:47:55 -0500

    Responsive, cambio seccion lo nuevo, y detalles

---
+++ js/scritp-image.js
@@ -0,0 +1,13 @@
+function cambiarImg1(elemento){
+    elemento.src="../img/producto_nuevo_3.png";
+  }
+  function restImg1(elemento){
+    elemento.src="../img/producto_nuevo_1.png"
+  }
+
+  function cambiarImg2(elemento){
+    elemento.src="../img/producto-5.png";
+  }
+  function restImg2(elemento){
+    elemento.src="../img/producto_nuevo_2.png"
+  }
